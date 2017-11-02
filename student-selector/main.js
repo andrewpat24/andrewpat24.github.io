5d4f63132a6e5a3aa63c8b415d7f197ed9e799a9
Andrew Patterson <apat24@mail.sfsu.edu>
2017-11-01 18:01:12 -0700

    adds students to period lists

--- student-selector/main.js
+++ student-selector/main.js
@@ -1,5 +1,5 @@
-let firstPeriodStudentList = ["Tommy", "Sarah", "James", "Tammy", "Jonathan", "Steven", "Amanda"];
-let secondPeriodStudentList = ["Andrew", "Micheal", "James", "Winnie"];
+let firstPeriodStudentList = ["Abby","Annie","Cally","Carina","Jimmy","David","Elizabeth","Gabby","Roxana","Ivy","Jackson","Jennifer","Jessica","Joshua","Kenny","Kimberly","Kobey","Kyra","Leanna","Lily","Megan","Melina","Michael","Rene","Ruby","Spencer C","Spencer L","Victoria","Wanwen","Weng Kei","Xitlaly","Yi Hao","Zesen","Zijun"];
+let secondPeriodStudentList = ["Alex","Baiyu","Bella","Benjamin","Brendan","Camille","Chris","Carys","Cindy","Emily","Gwendolyn","Jiaying","Emily","Jinghong","Kaitlyn","Kelly","Kelvin","Kevin","Khoi","Krystal","Edison","Lucas","Matthew","Michael","Paul","Raymond","Roxsanne","Sean","Tatsuya","Victoria","Joanna","Xinyi","Yingshi","Yunshi","Leo","Sadie"];
 
 function pickRandomStudent(studentList) {
 	let randomIndex = Math.floor((Math.random() * studentList.length) + 0);
